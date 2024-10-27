ba5565438b759f2b964dffff85fe4e893762035c
AlexP8on <118891272+AlexP8on@users.noreply.github.com>
2024-10-27 13:51:32 +0000

    Linking Project to Database

---
+++ src/main/java/ie/atu/bam/pool/Main.java
@@ -0,0 +1,12 @@
+package ie.atu.bam.pool;
+
+import java.sql.SQLException;
+public class Main {
+    public static void main(String[] args) {
+        String host = "localhost";
+        String user = "root";
+        String password = "Password";
+        String database = "bamdatabase";
+        String adminPassword = "admin";
+    }
+}
