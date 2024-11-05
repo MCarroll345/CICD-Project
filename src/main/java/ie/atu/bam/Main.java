afbc9f11d2e9efa423e5c0dfd62fe2ab83a03bf4
AlexP8on <118891272+AlexP8on@users.noreply.github.com>
2024-11-05 14:40:35 +0000

    Added dependencies to Pom

--- src/main/java/ie/atu/bam/Main.java
+++ src/main/java/ie/atu/bam/Main.java
@@ -1,6 +1,9 @@
-package ie.atu.bam.pool;
+package ie.atu.bam;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         String host = "localhost";
@@ -8,5 +11,8 @@ public class Main {
         String password = "Password";
         String database = "bamdatabase";
         String adminPassword = "admin";
+
+        DatabaseManager db = new DatabaseManager(host, user, password, database);
+
     }
 }
---
+++ src/main/java/ie/atu/bam/Standerd/DatabaseTester.java
@@ -0,0 +1,30 @@
+package ie.atu.bam.Standerd;
+import java.sql.*;
+public class DatabaseTester {
+        public static void main(String[] args) {
+
+            Connection connection = null;
+            try
+            {
+                // Load the driver class
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                // Create a connection to the database, hardcoding values for now.
+                connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/prem_app", "root", "Password");
+                System.out.println("Connection made to connection pool");
+            } catch (ClassNotFoundException | SQLException e) {
+                e.printStackTrace();
+            } finally
+            {
+                // Close the connection when finished
+                if (connection != null) {
+                    try
+                    {
+                        connection.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
