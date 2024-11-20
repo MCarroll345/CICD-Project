8dfcecc80aa7fa3f75bb4fdbceb5d934e36e981b
Mark Carroll <G00419267@atu.ie>
2024-11-20 15:49:02 +0000

    Updated Controller and services Added GEH

--- src/main/java/ie/atu/bam/CustomerServices.java
+++ src/main/java/ie/atu/bam/CustomerServices.java
@@ -2,8 +2,13 @@ package ie.atu.bam;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class CustomerServices {
+    private List<Customer> myList = new ArrayList<>();
+
     private CustomerRepository customerRepository;
 
     public CustomerServices(CustomerRepository customerRepository){
@@ -14,4 +19,16 @@ public class CustomerServices {
         customerRepository.save(customer);
         System.out.println("Customer created: " + customer);
     }
+
+    public Customer getCustomer(String username){
+        for(Customer c:myList){
+            if(c.getUsername() == username){
+                return getCustomer(username);
+            }
+        }
+
+
+        return new Customer();
+    }
 }
+
